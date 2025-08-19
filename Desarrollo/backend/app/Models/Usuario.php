910aca2d238eabc06a2f9bd083342d4ccdd093ad
Kehian Martins <kehianmartins2703@gmail.com>
2025-08-19 16:02:09 -0300

    funct (usuario): Agregado clase de Usuario

--- Desarrollo/backend/app/Models/Usuario.php
+++ Desarrollo/backend/app/Models/Usuario.php
@@ -2,22 +2,20 @@
 
 abstract class Usuario {
 
-    // protected string $nombre;
-    // protected string $apellido; 
-    // protected string $correo; 
-    // protected string $contrasena; 
-    // protected DateTime $fecha_nacimiento;
+    protected string $nombre;
+    protected string $apellido; 
+    protected string $correo; 
+    protected string $contrasena; 
+    protected DateTime $fecha_nacimiento;
 
-    // public function __construct(string $nombre, string $apellido, string $correo, string $contrasena, string $fecha_nacimiento) {
-    //     $this->nombre = $nombre;
-    //     $this->apellido = $apellido;
-    //     $this->correo = $correo;
-    //     $this->contrasena = $contrasena;
-    //     $this->fecha_nacimiento = new DateTime($fecha_nacimiento);
-    // }
-
-    public function __construct(protected string $nombre, protected string $apellido, protected string $correo, protected string $contrasena, protected DateTime $fecha_nacimiento) {
+    public function __construct(string $nombre, string $apellido, string $correo, string $contrasena, string $fecha_nacimiento) {
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->correo = $correo;
+        $this->contrasena = $contrasena;
+         $this->fecha_nacimiento = new DateTime($fecha_nacimiento);
     }
+
 
 
 }
