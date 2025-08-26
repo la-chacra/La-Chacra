406f7e9c8cee355c3b7eac5496f04d8df4a70b03
Kehian Martins <kehianmartins2703@gmail.com>
2025-08-26 14:56:28 -0300

    funct(models) LGC44: Modificada clase de usuario

--- Desarrollo/backend/app/Models/Cliente.php
+++ Desarrollo/backend/app/Models/Cliente.php
@@ -2,11 +2,6 @@
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-class Cliente extends Usuario {
-
-    public function __construct(int $usuario_id, int $nivelDeAcceso = 0, string $nombre, string $apellido, string $correo, string $contrasena, string $fechaNacimiento) {
-        parent::__construct($usuario_id, $nivelDeAcceso, $nombre, $apellido, $correo, $contrasena, $fechaNacimiento,);
-    }
-}
+class Cliente extends Usuario {}
 
 
