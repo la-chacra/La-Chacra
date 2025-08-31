081bb442d7d1309481fa3b7d1f28ac00cf8d8f61
BernardoArregin <arreginbernardo@gmail.com>
2025-08-31 14:36:48 -0300

    Cambios viejos, antes lo de Tiago

--- Desarrollo/frontend/postcss.config.js
+++ Desarrollo/frontend/postcss.config.js
@@ -4,10 +4,3 @@ export default {
     autoprefixer: {},
   },
 }
-
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  },
-};      
--- Desarrollo/frontend/src/App.jsx
+++ Desarrollo/frontend/src/App.jsx
@@ -1,26 +1,25 @@
-import { useState } from 'react'
+import React from 'react';
 
-import './App.css'
-import Header from "./Header"
-import Hero from "./Hero"
-import Horarios from "./Horarios"
-import MasVendidos from "./MasVendidos"
-import Mision from "./Mision"
-import Footer from "./Footer"
+import './App.css';
+import './index.css';
+import Header from "./Header";
+import Hero from "./Hero";
+import Horarios from "./Horarios";
+import MasVendidos from "./MasVendidos";
+import Mision from "./Mision";
+import Footer from "./Footer";
 
 function App() {
   return (
-    <>
-      <div>
-        <Header />
-        <Hero />
-        <Horarios />
-        <MasVendidos />
-        <Mision />
-        <Footer />
-      </div>
-    </>
-  )
+    <div>
+      <Header />
+      <Hero />
+      <Horarios />
+      <MasVendidos />
+      <Mision />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
