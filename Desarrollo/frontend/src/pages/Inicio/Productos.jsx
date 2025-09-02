43d45df879879c44aa2a65d01c96fa8177dc6e71
BernardoArregin <arreginbernardo@gmail.com>
2025-09-02 00:24:57 -0300

    Arreglo de las Secciones Horarios y Productos

--- Desarrollo/frontend/src/pages/Inicio/Productos.jsx
+++ Desarrollo/frontend/src/pages/Inicio/Productos.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 
+
 export default function Productos() {
   return (
-    <section className="bg-[#1c1c1c] text-white py-12">
+    <section className="bg-white-500 py-12">
       <div className="max-w-5xl mx-auto px-6 flex flex-col md:flex-row gap-8 items-center">
         
         {/* Imagen */}
         <img
           src="https://images.unsplash.com/photo-1501004318641-b39e6451bec6"
           alt="Productos orgánicos"
-          className="rounded-lg shadow-md w-full md:w-1/2"
+          className="bg-green w-full md:w-1/2 rounded-lg shadow-md"
         />
 
         {/* Texto */}
-        <div className="bg-[#2c2c2c] rounded-lg p-6">
-          <h2 className="bg-green-800  text-white inline-block px-3 py-1 rounded-md mb-4">
+        <div className="bg-[#2c2c2c] p-7 rounded-lg shadow-md">
+          <h2 className="bg-green-800 text-white px-3 py-1 rounded inline-block font-bold mb-4">
             Productos Orgánicos
           </h2>
           <p className="text-gray-300 leading-relaxed">
