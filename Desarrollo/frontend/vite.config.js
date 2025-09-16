50690f1af6bfd9db9173d9036c26b0ad456fb053
Kehian Martins <kehianmartins2703@gmail.com>
2025-09-16 11:21:35 -0300

    config(config) : Se agregó la capacidad de vite de que el servidor sea accesible desde la red.

--- Desarrollo/frontend/vite.config.js
+++ Desarrollo/frontend/vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permite que el servidor escuche en todas las interfaces
+    port: 5173, // Puerto en el que se ejecuta el servidor
+  },
 });
+
