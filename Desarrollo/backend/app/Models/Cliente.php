83cb5abebe66d41894b995fa87921c13fa8336b8
mateoparentini <parentinimateo@gmail.com>
2025-09-17 16:18:49 -0300

    funct(model): Comentario de metodos a realizar

--- Desarrollo/backend/app/Models/Cliente.php
+++ Desarrollo/backend/app/Models/Cliente.php
@@ -5,3 +5,5 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 use database;
 
 class Cliente extends Usuario {}
+
+// actualizarDisponibilidad(),generarReporte(),obtenerTopClientes(),eliminarComanda(), todo admin
