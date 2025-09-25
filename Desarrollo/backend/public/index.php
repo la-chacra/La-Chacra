6fef09a2b5d2dd5364d49fda914f4c0c3538942b
Kehian Martins <kehianmartins2703@gmail.com>
2025-09-25 11:08:19 -0300

    funct(Router): Agregado clase de enrutador.
    Se agregó la clase de enrutador, encargado de manejar las rutas y enlazarlas con su debido controlador. Ej: Al entrar en localhost/login, lo enlaza con LoginController. Se especificaron las rutas iniciales en index.php. Se modifico el namespace de Database.php.

--- Desarrollo/backend/public/index.php
+++ Desarrollo/backend/public/index.php
@@ -4,15 +4,19 @@ error_reporting(E_ALL); // reportar todos los errores
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
+use Router\Router;
 use Config\Database;
 use App\Models\ModeloBase;
-use App\Models\Administrador;
+use App\Controllers\LoginController;
+use App\Controllers\RegistroController;
+use App\Models\Administrador; // remover despues
 
-echo "hola";
+$router = new Router();
 
-$db = new Database();
+$router->post("/login", [LoginController::class, "login"]);
+$router->post("/registro", [RegistroController::class, "login"]);
 
-ModeloBase::setDB($db);
+$router->despachar();
 
 // Testing
 // $admin = new Administrador("Kehian", "Martins", "asdasd@gmail.com", "9asdkas", "1987-03-20");
