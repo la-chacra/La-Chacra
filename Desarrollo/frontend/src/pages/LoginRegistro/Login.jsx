bda0e8b876e702b42e29f10ab01eea9a42da7ac6
Kehian Martins <kehianmartins2703@gmail.com>
2025-09-25 15:26:03 -0300

    En progeso LoginController

--- Desarrollo/frontend/src/pages/LoginRegistro/Login.jsx
+++ Desarrollo/frontend/src/pages/LoginRegistro/Login.jsx
@@ -10,15 +10,72 @@ const Auth = () => {
   const [isSignUp, setIsSignUp] = useState(false);
   const [hoveredPanel, setHoveredPanel] = useState(null);
 
-  
-
-  /* Estados para Registro */
+  // Estados para formulario de Registro
   const [nombre, setNombre] = useState("");
   const [apellido, setApellido] = useState("");
-  const [regEmail, setRegEmail] = useState("");
-  const [regPassword, setRegPassword] = useState("");
-  const [fecha, setFecha] = useState("");
+  const [regCorreo, setRegCorreo] = useState("");
+  const [regContrasena, setRegContrasena] = useState("");
+  const [confirmarContrasena, setConfirmarContrasena] = useState("");
+  const [fechaNacimiento, setFechaNacimiento] = useState("");
 
+  // Estados para formulario de Login
+  const [loginCorreo, setLoginCorreo] = useState("");
+  const [loginContrasena, setLoginContrasena] = useState("");
+
+    const handleLogin = async (e) => {
+    e.preventDefault(); // evita recargar la página
+
+    const dataSend = {
+      correo: loginCorreo,
+      contrasena: loginContrasena
+    };
+
+    const res = await fetch("http://localhost:8000/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(dataSend),
+    });
+
+    const dataRes = await res.json();
+    console.log("Login response:", data);
+
+    if (dataRes.status === "ok") {
+      localStorage.setItem("token", dataRes.token);
+      alert("Login exitoso");
+    } else {
+      alert("Error: " + dataRes.message);
+    }
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    
+    if (regContrasenaontrasena !== confirmarContrasena) {
+      alert("Las contraseñas no coinciden");
+      return;
+    }
+
+    const res = await fetch("http://localhost:8000/registro", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        nombre,
+        apellido,
+        correo: regCorreo,
+        fecha,
+        contrasena: regContrasena,
+      }),
+    });
+
+    const dataRes = await res.json();
+    console.log("Register response:", dataRes);
+
+    if (dataRes.status === "ok") {
+      alert("Usuario registrado");
+    } else {
+      alert("Error: " + dataRes.message);
+    }
+  };
 
   return (
     <>
@@ -28,24 +85,26 @@ const Auth = () => {
           
           {/* FORMULARIOS PARA DESKTOP */}
           <div className="form-container sign-up-container">
-            <form>
+            <form onSubmit={handleRegister}>
               <h2 className="auth-title">REGISTRO</h2>
               <div className="input-group">
-                <input type="text" name="nombre" placeholder="Nombre" />
-                <input type="text" name="apellido" placeholder="Apellido" />
+                <input type="text" name="nombre" placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)}/>
+                <input type="text" name="apellido" placeholder="Apellido" value={apellido} onChange={(e) => setApellido(e.target.value)}/>
               </div>
               <div className="input-group">
-                <input type="email" name="RegCorreo" placeholder="Correo electrónico" />
+                <input type="email" name="regCorreo" placeholder="Correo electrónico" value={regCorreo} onChange={(e) => setRegCorreo(e.target.value)} />
                 <input
                   type="text"
                   name="fechaNacimiento"
                   placeholder="__/__/____"
                   onFocus={(e) => (e.target.type = "date")}
                   onBlur={(e) => (e.target.type = "text")}
+                  value={fechaNacimiento}
+                  onChange={(e) => setFechaNacimiento(e.target.value)}
                 />
               </div>
-              <input type="password" name="RegContrasena" placeholder="Contraseña" />
-              <input type="password" name="confContrasena" placeholder="Confirme su contraseña" />
+              <input type="password" name="regContrasena" placeholder="Contraseña" value={regContrasena} onChange={(e) => setRegContrasena(e.target.value)}/>
+              <input type="password" name="confirmarContrasena" placeholder="Confirme su contraseña" value={confirmarContrasena} onChange={(e) => setConfirmarContrasena(e.target.value)}/>
               <button type="submit" className="auth-btn">
                 REGISTRARSE
               </button>
@@ -62,12 +121,12 @@ const Auth = () => {
           </div>
 
           <div className="form-container sign-in-container">
-            <form>
+            <form onSubmit={handleLogin}>
               <h2 className="auth-title">INICIO DE SESIÓN</h2>
               <label for="LogCorreo">Correo Electrónico</label>
-              <input type="email" id="LogCorreo" name="LoginCorreo" placeholder="Ingrese su correo electrónico" />
+              <input type="email" id="LogCorreo" name="loginCorreo" placeholder="Ingrese su correo electrónico" />
               <label for="LogContrasena">Contraseña</label>
-              <input type="password" id="LogContrasena" name="LoginContrasena" placeholder="Ingrese su contraseña" />
+              <input type="password" id="LogContrasena" name="loginContrasena" placeholder="Ingrese su contraseña" />
               <button type="submit" className="auth-btn">
                 INICIAR SESIÓN
               </button>
@@ -131,24 +190,26 @@ const Auth = () => {
           {/* FORMULARIOS MÓVIL */}
           <div className="form-container-mobile">
             {isSignUp ? (
-              <form className="sign-up-container-mobile">
+              <form className="sign-up-container-mobile" onSubmit={handleRegister}>
                 <h2 className="auth-title">REGISTRO</h2>
                 <div className="input-group">
-                  <input type="text" name="nombre" placeholder="Nombre" />
-                  <input type="text" name="apellido" placeholder="Apellido" />
+                  <input type="text" name="nombre" placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+                  <input type="text" name="apellido" placeholder="Apellido" value={apellido} onChange={(e) => setApellido(e.target.value)} />
                 </div>
                 <div className="input-group">
-                  <input type="email" name="RegCorreo" placeholder="Correo" />
+                  <input type="email" name="regCorreo" placeholder="Correo" value={regCorreo} onChange={(e) => setRegCorreo(e.target.value)} />
                   <input
                     type="text"
                     name="fechaNacimiento"
                     placeholder="__/__/____"
                     onFocus={(e) => (e.target.type = "date")}
                     onBlur={(e) => (e.target.type = "text")}
+                    value={fechaNacimiento}
+                    onChange={(e) => setFechaNacimiento(e.target.value)}
                   />
                 </div>
-                <input type="password" name="RegContrasena" placeholder="Contraseña" />
-                <input type="password" name="confContrasena" placeholder="Confirme su contraseña" />
+                <input type="password" name="regContrasena" placeholder="Contraseña" value={regContrasena} onChange={(e) => setRegContrasena(e.target.value)} />
+                <input type="password" name="confContrasena" placeholder="Confirme su contraseña" value={confirmarContrasena} onChange={(e) => setConfirmarContrasena(e.target.value)} />
                 <button type="submit" className="auth-btn">
                   REGISTRARSE
                 </button>
@@ -163,12 +224,12 @@ const Auth = () => {
                 </div>
               </form>
             ) : (
-              <form className="sign-in-container-mobile">
+              <form className="sign-in-container-mobile" onSubmit={handleLogin}>
                 <h2 className="auth-title">INICIO DE SESIÓN</h2>
                 <label>Correo Electrónico</label>
-                <input type="email" name="LoginCorreo" placeholder="Ingrese su correo electrónico" />
+                <input type="email" name="loginCorreo" placeholder="Ingrese su correo electrónico" value={loginCorreo} onChange={(e) => setLoginCorreo(e.target.value)} />
                 <label>Contraseña</label>
-                <input type="password" name="LoginContrasena" placeholder="Ingrese su contraseña" />
+                <input type="password" name="loginContrasena" placeholder="Ingrese su contraseña" value={loginContrasena} onChange={(e) => setLoginContrasena(e.target.value)} />
                 <button type="submit" className="auth-btn">
                   INICIAR SESIÓN
                 </button>
