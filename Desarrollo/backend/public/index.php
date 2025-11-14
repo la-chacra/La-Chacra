920fdbd51cb45553ab5fedf918c2288b9792dbdb
Kehian Martins <kehianmartins2703@gmail.com>
2025-11-14 04:13:17 -0300

    Arregladas URL

--- Desarrollo/backend/public/index.php
+++ Desarrollo/backend/public/index.php
@@ -104,11 +104,13 @@ $router->delete('/api/productos-menu/desactivar/{id}', [PlatoController::class, 
 
 
 
-$allowedOrigin = "http://localhost:5173";
-header("Access-Control-Allow-Origin: $allowedOrigin");
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '*';
+
+header("Access-Control-Allow-Origin: $origin");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
